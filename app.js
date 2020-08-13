c6e185ed9cd8f71dcb88a0dacab0f3081597d9e3
Corey Burkett <coreyburkett22@gmail.com>
2020-08-13 15:56:05 -0700

    created first prompt

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
-const axios = require("axios");
-const Employee = require("./lib/Employee");
+// const fs = require("fs");
+// const axios = require("axios");
+// const Employee = require("./lib/Employee");
 
 inquirer.prompt([
-    // refer to README line 20 for prompts
     {
-        type: "",
-        name: "",
-        message: "",
+        type: "input",
+        name: "id",
+        message: "What is your employee ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your employee email?"
+    },
+    {
+        type: "checkbox",
+        name: "role",
+        message: "What is your role?",
         choices: [
-            "choice1",
-            "choice2"
+            "Manager",
+            "Engineer",
+            "Intern"
         ]
     }
-    // "name"
-]).then(function({ name }) {
-    // if "name" == certain "choice"
-    if (name == choice1){
-        // run function
-    // if "name" == certain "choice"
-    }else if (name == choice2){
-        // run function
-    }
+]).then(function({ role }) {
+        if (role == "Manager"){
+            // run function
+        }else if (role == "Engineer"){
+            // run function
+        }else if (role == "Intern"){
+            // run function
+        }
+
 });
