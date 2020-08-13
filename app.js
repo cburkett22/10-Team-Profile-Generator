922c283f94accbca746e51c66d34c061e7424f7e
Corey Burkett <coreyburkett22@gmail.com>
2020-08-13 15:30:21 -0700

    created starter code and connected files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
+const Employee = require("./lib/Employee");
 
 inquirer.prompt([
     // refer to README line 20 for prompts
@@ -9,8 +10,8 @@ inquirer.prompt([
         name: "",
         message: "",
         choices: [
-            "",
-            ""
+            "choice1",
+            "choice2"
         ]
     }
     // "name"
@@ -19,7 +20,7 @@ inquirer.prompt([
     if (name == choice1){
         // run function
     // if "name" == certain "choice"
-    }else if (name == choice1){
+    }else if (name == choice2){
         // run function
     }
 });
