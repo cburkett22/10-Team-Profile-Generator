418355213cb028f9067a6aca0cd145d25b7c45a0
Corey Burkett <coreyburkett22@gmail.com>
2020-08-14 15:52:25 -0700

    edited and completed employee prompts

--- app.js
+++ app.js
@@ -6,60 +6,126 @@ const inquirer = require("inquirer");
 inquirer.prompt([
     {
         type: "input",
+        name: "name",
+        message: "Enter your name (manager):"
+    },
+    {
+        type: "input",
         name: "id",
-        message: "What is your employee ID number?"
+        message: "Enter your ID number (manager):"
     },
     {
         type: "input",
         name: "email",
-        message: "What is your employee email?"
+        message: "Enter your email (manager):"
     },
     {
-        type: "checkbox",
-        name: "role",
-        message: "What is your role?",
-        choices: [
-            "Manager",
-            "Engineer",
-            "Intern"
-        ]
+        type: "input",
+        name: "office",
+        message: "Enter your office number (manager):"
     }
-]).then(function({ role }) {
-        if (role == "Manager"){
-            // run function
-            promptGithub();
-        }else if (role == "Engineer"){
-            // run function
-            promptGithub();
-        }else if (role == "Intern"){
-            // run function
-            promptSchool();
-        }
+]).then(function(res) {
+    // push id, email, and office number to html card
+    // $("#name").text(res.name);
+    // $("#id").text("ID: " + res.id);
+    // $("#email").text("Email: " + res.email);
+    // $("#office").text("Office: " + res.office);
 
+    promptEngineer();
 });
 
-function promptGithub() {
+function promptEngineer() {
     inquirer.prompt([
         {
             type: "input",
-            name: "username",
-            message: "Enter your GitHub username:"
+            name: "name",
+            message: "Enter the engineers name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the engineers ID number:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the engineers company email:"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter the engineers GitHub username:"
         }
-    ]).then(function(userInput){
-        // push username to html card
-        // $("#id").text("GitHub: " + userInput.username);
+    ]).then(function(res) {
+        inquirer.prompt([
+            {
+                type: "checkbox",
+                name: "moreEmp",
+                message: "Are there anymore engineers?",
+                choices: [
+                    "Yes",
+                    "No"
+                ]
+            }
+        ]).then(function(res) {
+            if (res.moreEmp == "Yes"){
+                promptEngineer();
+            }else{
+                promptIntern();
+            }
+        });
+
+        // push id, email, and github username to html card
+        // $("#name").text(res.name);
+        // $("#id").text("ID: " + res.id);
+        // $("#email").text("Email: " + res.email);
+        // $("#github").text("Github: " + res.github);
     });
 };
 
-function promptSchool() {
+function promptIntern() {
     inquirer.prompt([
         {
             type: "input",
-            name: "school",
-            message: "What college do you attend?"
+            name: "name",
+            message: "Enter the interns name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the interns ID number:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the interns company email:"
+        },
+        {
+            type: "input",
+            name: "college",
+            message: "What college does the intern attend?"
         }
-    ]).then(function(userInput){
-        // push username to html card
-        // $("#id").text("College: " + userInput.school);
+    ]).then(function(res) {
+        inquirer.prompt([
+            {
+                type: "checkbox",
+                name: "moreInt",
+                message: "Are there anymore interns?",
+                choices: [
+                    "Yes",
+                    "No"
+                ]
+            }
+        ]).then(function(res) {
+            if (res.moreInt == "Yes"){
+                promptIntern();
+            }
+        });
+
+        // push id, email, and college username to html card
+        // $("#name").text(res.name);
+        // $("#id").text("ID: " + res.id);
+        // $("#email").text("Email: " + res.email);
+        // $("#college").text("College: " + res.college);
     });
 };
