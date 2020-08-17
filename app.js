5e5fbbe03140d41dc589495c2ac11db1d0ac9048
Corey Burkett <coreyburkett22@gmail.com>
2020-08-17 16:29:41 -0700

    completed prompts, classes, and rendered html

--- app.js
+++ app.js
@@ -1,131 +1,142 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const axios = require("axios");
-// const Employee = require("./lib/Employee");
+const path = require("path");
+const fs = require("fs");
 
-inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "Enter your name (manager):"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Enter your ID number (manager):"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter your email (manager):"
-    },
-    {
-        type: "input",
-        name: "office",
-        message: "Enter your office number (manager):"
-    }
-]).then(function(res) {
-    // push id, email, and office number to html card
-    // $("#name").text(res.name);
-    // $("#id").text("ID: " + res.id);
-    // $("#email").text("Email: " + res.email);
-    // $("#office").text("Office: " + res.office);
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-    promptEngineer();
-});
+const render = require("./lib/htmlRenderer");
 
-function promptEngineer() {
+const myTeam = [];
+
+promptEmployee();
+function promptEmployee() {
     inquirer.prompt([
+        {
+            type: "checkbox",
+            name: "role",
+            message: "What is the employee's role you are creating?",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern"
+            ]
+        },
         {
             type: "input",
             name: "name",
-            message: "Enter the engineers name:"
+            message: "Enter employee's name:"
         },
         {
             type: "input",
             name: "id",
-            message: "Enter the engineers ID number:"
+            message: "Enter employee's ID:"
         },
         {
             type: "input",
             name: "email",
-            message: "Enter the engineers company email:"
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Enter the engineers GitHub username:"
+            message: "Enter employee's email:"
         }
-    ]).then(function(res) {
-        inquirer.prompt([
-            {
-                type: "checkbox",
-                name: "moreEmp",
-                message: "Are there anymore engineers?",
-                choices: [
-                    "Yes",
-                    "No"
-                ]
-            }
-        ]).then(function(res) {
-            if (res.moreEmp == "Yes"){
-                promptEngineer();
-            }else{
-                promptIntern();
-            }
-        });
+    ]).then(function(preInfo) {
+        if (preInfo.role == "Manager") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "office",
+                    message: "Enter the managers office number:"
+                },
+                {
+                    type: "checkbox",
+                    name: "addMem",
+                    message: "Would you like to add another team member?",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ]).then(function(res) {
+                preInfo.office = res.office;
 
-        // push id, email, and github username to html card
-        // $("#name").text(res.name);
-        // $("#id").text("ID: " + res.id);
-        // $("#email").text("Email: " + res.email);
-        // $("#github").text("Github: " + res.github);
-    });
-};
+                const manager = new Manager(preInfo.name, preInfo.id, preInfo.email, preInfo.office);
+                myTeam.push(manager);
 
-function promptIntern() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the interns name:"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Enter the interns ID number:"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Enter the interns company email:"
-        },
-        {
-            type: "input",
-            name: "college",
-            message: "What college does the intern attend?"
+                if (res.addMem == "Yes") {
+                    promptEmployee();
+                } else {
+                    fs.writeFile(outputPath, render(myTeam), function(err){
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
+        } else if (preInfo.role == "Engineer") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "github",
+                    message: "Enter the Engineer's GitHub username:"
+                },
+                {
+                    type: "checkbox",
+                    name: "addMem",
+                    message: "Would you like to add another team member?",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ]).then(function(res) {
+                preInfo.github = res.github;
+
+                const engineer = new Engineer(preInfo.name, preInfo.id, preInfo.email, preInfo.github);
+                myTeam.push(engineer);
+
+                if (res.addMem == "Yes") {
+                    promptEmployee();
+                } else {
+                    fs.writeFile(outputPath, render(myTeam), function(err){
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
+        } else if (preInfo.role == "Intern") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "college",
+                    message: "Enter the Intern's college:"
+                },
+                {
+                    type: "checkbox",
+                    name: "addMem",
+                    message: "Would you like to add another team member?",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ]).then(function(res) {
+                preInfo.college = res.college;
+
+                const intern = new Intern(preInfo.name, preInfo.id, preInfo.email, preInfo.college);
+                myTeam.push(intern);
+
+                if (res.addMem == "Yes") {
+                    promptEmployee();
+                } else {
+                    fs.writeFile(outputPath, render(myTeam), function(err){
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
         }
-    ]).then(function(res) {
-        inquirer.prompt([
-            {
-                type: "checkbox",
-                name: "moreInt",
-                message: "Are there anymore interns?",
-                choices: [
-                    "Yes",
-                    "No"
-                ]
-            }
-        ]).then(function(res) {
-            if (res.moreInt == "Yes"){
-                promptIntern();
-            }
-        });
-
-        // push id, email, and college username to html card
-        // $("#name").text(res.name);
-        // $("#id").text("ID: " + res.id);
-        // $("#email").text("Email: " + res.email);
-        // $("#college").text("College: " + res.college);
     });
 };
